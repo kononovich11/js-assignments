980157f92f03c39e1636e958029b99d7b9f4d870
Kononovich Halina <kononovich1703@gmail.com>
2020-10-21 21:37:19 +0300

    add solution on date tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 
@@ -56,7 +56,10 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   const year = date.getFullYear();
+   const needDate = new Date(year, 1, 29);
+   if (needDate.getMonth() === 1) return true;
+   return false;
 }
 
 
@@ -76,7 +79,19 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let days = endDate.getDate() - startDate.getDate();
+   let hours = endDate.getHours() - startDate.getHours();
+   let minutes = endDate.getMinutes() - startDate.getMinutes();
+   let seconds = endDate.getSeconds() - startDate.getSeconds();
+   let ms = endDate.getMilliseconds() - startDate.getMilliseconds();
+
+   days == 1 ? hours += 24 : null;
+   hours = hours < 10 ? '0' + hours : hours;
+   minutes = minutes < 10 ? '0' + minutes : minutes;
+   seconds = seconds < 10 ? '0' + seconds : seconds;
+   ms = ms < 10 ? '0' + '0' + ms : ms;
+
+   return`${hours}:${minutes}:${seconds}.${ms}`;
 }
 
 
